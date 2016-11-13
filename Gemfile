6e3caaf530bb90c089b1f656fd9fe4155c89a322
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2016-11-12 18:46:06 -0800

    Adding controller + view logic for last witnessed at

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby "2.2.2"
 
 gem "sinatra", "~> 1.4", ">= 1.4.7"
 gem "activerecord"
+gem "activesupport", "~> 5.0", ">= 5.0.0.1"
 gem "sinatra-activerecord"
 gem "sqlite3"
 gem "rake"
+gem "pry"
--- app/moghy.rb
+++ app/moghy.rb
@@ -1,8 +1,15 @@
 require "sinatra"
 require "sinatra/activerecord"
+require "active_support"
+
+require_relative "models/witness"
 
 set :database, "sqlite3:moghy.sqlite3"
+Time.zone = "UTC"
 
 get "/" do
+  @witness = Witness.order(:witnessed_at).last
+  @drunk = @witness ? @witness.witnessed_at > 4.hours.ago : false
+
   erb :index
 end
