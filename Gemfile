dc87acabd4117f69e4c5f6d4ced3e8bc031015d9
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2016-11-14 01:04:16 -0800

    Require name

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 ruby "2.2.2"
 
 gem "sinatra", "~> 1.4", ">= 1.4.7"
+gem "sinatra-flash"
 gem "activerecord"
 gem "activesupport", "~> 5.0", ">= 5.0.0.1"
 gem "sinatra-activerecord"
--- app/moghy.rb
+++ app/moghy.rb
@@ -1,6 +1,7 @@
 require "zlib"
 require "sinatra"
 require "sinatra/activerecord"
+require "sinatra/flash"
 require "active_support"
 
 require_relative "models/witness"
@@ -8,6 +9,8 @@ require_relative "models/witness"
 DEFAULT_DB = "postgres://localhost/moghy"
 set :database, DEFAULT_DB
 ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || DEFAULT_DB)
+
+enable :sessions
 
 Time.zone = "UTC"
 
@@ -26,7 +29,12 @@ post "/witness" do
   Time.zone = "UTC"
 
   name = params[:name]
-  name = nil if name == ""
+  if name.nil? || name == ""
+    require 'pry'; binding.pry
+    flash[:error] = "FOOOOOO"
+    redirect to("/")
+    return
+  end
 
   Witness.create!(name: name, witnessed_at: Time.zone.now)
   redirect to("/")
